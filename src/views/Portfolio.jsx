2595a3893d7f95698f4788eb44791a996d504d37
AnthonyMcCullough <mccullougha583@gmail.com>
2023-11-27 09:42:38 -0700

    created skills section

--- src/views/Portfolio.jsx
+++ src/views/Portfolio.jsx
@@ -3,10 +3,26 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+
+
 import pic from '../assets/profilePic.png';
 import linkedin from '../assets/linkedin.png';
 import message from '../assets/message.png';
 import github from '../assets/github.png';
+
+import bootstrap from '../assets/bootstrap.png'
+import css from '../assets/css.png'
+import express from '../assets/express.png'
+import html from '../assets/html.png'
+import javascript from '../assets/javascript.png'
+import jquery from '../assets/jquery.png'
+import mongoDB from '../assets/mongoDB.png'
+import mongoose from '../assets/mongoose.png'
+import node from '../assets/node.png'
+import oop from '../assets/oop.png'
+import sass from '../assets/sass.png'
+import react from '../assets/react.png'
+
 
 
 
@@ -83,18 +99,35 @@ export default function Portfolio({ }) {
                             </div>
 
 
-                            {/* <div className='contentDiv'>
+                            <div className='contentDiv'>
                                 <h1 className='title2'>Skills</h1>
                                 <div className="text">
-                                   Hello , my name is Anthony McCullough . 
-                                   Im a web developer from Phoenix,AZ that has a passion for designing, 
-                                   constructing, and debugging mobile responsive web applications. 
-                                   I want to continue my education in the technical indsurty to include creating video games and virtual reality.
-                                    My other hobbies include sports, hiking, fishing, excercising, and eating.
+                               
+                               <img src={html} className='iconPicSize'></img>
+                               <img src={css} className='iconPicSize'></img>
+                               <img src={javascript} className='iconPicSize'></img>
+                               <img src={oop} className='iconPicSize'></img>
+                               <img src={react} className='iconPicSize'></img>
+                               <img src={bootstrap} className='iconPicSize'></img>
 
                                     
                                 </div>
-                            </div> */}
+
+                                <div className="text">
+                                
+                                <img src={sass} className='iconPicSize'></img>
+                               <img src={jquery} className='iconPicSize'></img>
+                               <img src={node} className='iconPicSize'></img>
+                               <img src={express} className='iconPicSize'></img>
+                               <img src={mongoose} className='iconPicSize'></img>
+                               <img src={mongoDB} className='iconPicSize'></img>
+                               
+
+                                    
+                                </div>
+
+
+                            </div>
 
 
 
