acdbfd4727564a1f35f2891bbab43bfdf5a4e776
AnthonyMcCullough <mccullougha583@gmail.com>
2023-11-30 04:27:47 -0700

    created projects section

--- src/views/Portfolio.jsx
+++ src/views/Portfolio.jsx
@@ -1,8 +1,11 @@
 
-import React, { useState} from 'react';
+import React, { useState, useRef  } from 'react';
 // import axios from 'axios';
 import { Link } from 'react-router-dom';
-import AnchorLink  from 'react-anchor-link-smooth-scroll';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+
+
+import emailjs from '@emailjs/browser';
 
 
 
@@ -34,19 +37,39 @@ import tripPlan from '../assets/tripPlan.png'
 
 const Portfolio = () => {
 
-    const [mainPic,setMainPic] = useState(eCommerce)
-
-
-
-
-    const displayLargeImage  = (mainPicPath) => {
+    const [mainPic, setMainPic] = useState(eCommerce);
     
+    const [name,setName] = useState("")
+    const [email,setEmail] = useState("")
+    const [message2,setMessage2] = useState("")
+
+
+
+
+    const displayLargeImage = (mainPicPath) => {
+
         setMainPic(mainPicPath)
-        
-    
-       
-    
+
+
+
+
     }
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_0rzl2fh', 'template_vmk2ut6', form.current, 'MgA8ahlt-dQ5O4lvQ')
+        .then((result) => {
+            console.log(result.text);
+            setName("")
+            setEmail("")
+            setMessage2("")
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
 
 
 
@@ -101,46 +124,46 @@ const Portfolio = () => {
                 <div className='mainFrame2'>
                     <div className='contentSection'>
                         <div className='content'>
-                            
+
                             <div className='contentDiv'>
                                 <h1 className='title2'>About Me</h1>
                                 <div className="text">
-                                   Hello , my name is Anthony McCullough . 
-                                   Im a web developer from Phoenix,AZ that has a passion for designing, 
-                                   constructing, and debugging mobile responsive web applications. 
-                                   I want to continue my education in the technical indsurty to include creating video games and virtual reality.
+                                    Hello , my name is Anthony McCullough .
+                                    Im a web developer from Phoenix,AZ that has a passion for designing,
+                                    constructing, and debugging mobile responsive web applications.
+                                    I want to continue my education in the technical indsurty to include creating video games and virtual reality.
                                     My other hobbies include sports, hiking, fishing, excercising, and eating.
 
-                                    
+
                                 </div>
                             </div>
-                                   
+
 
                             <div className='contentDiv'>
                                 <h1 className='title2'>Skills</h1>
                                 <div className="text">
-                               
-                               <img alt="alt" src={html} className='iconPicSize'></img>
-                               <img alt="alt"  src={css} className='iconPicSize'></img>
-                               <img alt="alt"  src={javascript} className='iconPicSize'></img>
-                               <img alt="alt"  src={oop} className='iconPicSize'></img>
-                               <img alt="alt"  src={react} className='iconPicSize'></img>
-                               <img alt="alt"  src={bootstrap} className='iconPicSize'></img>
 
-                                    
+                                    <img alt="alt" src={html} className='iconPicSize'></img>
+                                    <img alt="alt" src={css} className='iconPicSize'></img>
+                                    <img alt="alt" src={javascript} className='iconPicSize'></img>
+                                    <img alt="alt" src={oop} className='iconPicSize'></img>
+                                    <img alt="alt" src={react} className='iconPicSize'></img>
+                                    <img alt="alt" src={bootstrap} className='iconPicSize'></img>
+
+
                                 </div>
 
                                 <div className="text">
-                                
-                                <img alt="alt"  src={sass} className='iconPicSize'></img>
-                               <img alt="alt"  src={jquery} className='iconPicSize'></img>
-                               <img alt="alt"  src={node} className='iconPicSize'></img>
-                               <img alt="alt"  src={express} className='iconPicSize'></img>
-                               <img alt="alt"  src={mongoose} className='iconPicSize'></img>
-                               <img alt="alt"  src={mongoDB} className='iconPicSize'></img>
-                               
 
-                                    
+                                    <img alt="alt" src={sass} className='iconPicSize'></img>
+                                    <img alt="alt" src={jquery} className='iconPicSize'></img>
+                                    <img alt="alt" src={node} className='iconPicSize'></img>
+                                    <img alt="alt" src={express} className='iconPicSize'></img>
+                                    <img alt="alt" src={mongoose} className='iconPicSize'></img>
+                                    <img alt="alt" src={mongoDB} className='iconPicSize'></img>
+
+
+
                                 </div>
 
 
@@ -151,44 +174,63 @@ const Portfolio = () => {
                             <div className='contentDiv'>
                                 <h1 className='title2'>Projects</h1>
                                 <div className="text2">
-                               
-                               <img alt="alt"  src={mainPic} className='bigPicSize'></img>
-                               
 
-                                    
+                                    <img alt="alt" src={mainPic} className='bigPicSize'></img>
+
+
+
                                 </div>
 
-         
+
 
                                 <div className="picRow">
-                               <div className='picLinkColumn'>
-                               <div className='miniTitle'>E-Commerce Store </div>
-                                <img alt="alt"  src={eCommerce} onClick={()=>displayLargeImage(eCommerce)} className='projectPicSize'></img>
-                                <Link to="https://github.com/AnthonyMcCullough/E-CommerceStore" className='link2'>GitHub</Link>
+                                    <div className='picLinkColumn'>
+                                        <div className='miniTitle'>E-Commerce Store </div>
+                                        <img alt="alt" src={eCommerce} onClick={() => displayLargeImage(eCommerce)} className='projectPicSize'></img>
+                                        <Link to="https://github.com/AnthonyMcCullough/E-CommerceStore" className='link2'>GitHub</Link>
+                                    </div>
+
+                                    <div className='picLinkColumn'>
+                                        <div className='miniTitle'>Personal Library </div>
+
+                                        <img alt="alt" src={library} onClick={() => displayLargeImage(library)} className='projectPicSize'></img>
+                                        <Link to="https://github.com/AnthonyMcCullough/PersonalLibrary" className='link2'>GitHub</Link>
+                                    </div>
+
+                                    <div className='picLinkColumn'>
+                                        <div className='miniTitle'>Trip Planner</div>
+                                        <img alt="alt" src={tripPlan} onClick={() => displayLargeImage(tripPlan)} className='projectPicSize'></img>
+                                        <Link to="https://github.com/AnthonyMcCullough/TripPlanner" className='link2'>GitHub</Link>
+                                    </div>
+
+                                    <div className='picLinkColumn'>
+                                        <div id="projects" className='miniTitle'>Calculator</div>
+                                        <img alt="alt" src={calculator} onClick={() => displayLargeImage(calculator)} className='projectPicSize'></img>
+                                        <Link to="https://github.com/AnthonyMcCullough/Calculator" className='link2'>GitHub</Link>
+                                    </div>
+
                                 </div>
 
-                                <div className='picLinkColumn'>
-                                <div className='miniTitle'>Personal Library </div>
-                          
-                                <img alt="alt"  src={library} onClick={()=>displayLargeImage(library)} className='projectPicSize'></img>
-                                <Link to="https://github.com/AnthonyMcCullough/PersonalLibrary" className='link2'>GitHub</Link>
-                                </div>   
-                                
-                                <div className='picLinkColumn'>
-                                <div className='miniTitle'>Trip Planner</div>
-                                <img alt="alt"  src={tripPlan} onClick={()=>displayLargeImage(tripPlan)} className='projectPicSize'></img>
-                                <Link to="https://github.com/AnthonyMcCullough/TripPlanner" className='link2'>GitHub</Link>
-                                </div>    
-                                
-                                <div className='picLinkColumn'>
-                                <div id="projects" className='miniTitle'>Calculator</div>
-                                <img alt="alt"  src={calculator} onClick={()=>displayLargeImage(calculator)}className='projectPicSize'></img>
-                                <Link to="https://github.com/AnthonyMcCullough/Calculator" className='link2'>GitHub</Link>
-                                </div>
-          
-                                </div>
+
+                            </div>
 
 
+                            <div className='contentDiv'>
+                                <h1 className='title2'>Contact Me</h1>
+                                <div className="text3">
+                                    
+                                    <form ref={form} onSubmit={sendEmail}>
+                                        <label className='font'>Name:</label>
+                                        <input className='inputSize font' type="text" name="user_name" value={name} onChange={(e)=>setName(e.target.value)} />
+                                        <label className='marg font'>Email:</label>
+                                        <input className='inputSize font' type="email" name="user_email" value={email} onChange={(e)=>setEmail(e.target.value)} />
+                                        <label className='marg font'>Message:</label>
+                                        <textarea className='textareaSize font' name="message" value={message2} onChange={(e)=>setMessage2(e.target.value)} />
+                                        <input className='marg font btn2' type="submit" value="Send" />
+                                    </form>
+
+
+                                </div>
                             </div>
 
 
@@ -201,27 +243,27 @@ const Portfolio = () => {
 
 
 
-                      
+
 
 
 
                     </div>
                     <div className='navSection'>
-                        
-                       <AnchorLink href="#aboutMe" className='link'>
-                        <button id="1" className='btn'>About Me</button>
+
+                        <AnchorLink href="#aboutMe" className='link'>
+                            <button id="1" className='btn'>About Me</button>
                         </AnchorLink>
-                        
+
                         <AnchorLink href="#skills" className='link'>
-                        <button id="2" className='btn'>Skills</button>
+                            <button id="2" className='btn'>Skills</button>
                         </AnchorLink>
 
                         <AnchorLink href="#projects" className='link'>
-                        <button id="3" className='btn'>Projects</button>
+                            <button id="3" className='btn'>Projects</button>
                         </AnchorLink>
-                        
+
                         <AnchorLink href="#contactForm" className='link'>
-                        <button className='btn'>Contact</button>
+                            <button className='btn'>Contact</button>
                         </AnchorLink>
 
                     </div>
